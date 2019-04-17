662b1f245ab9c0920a7918c61e40e7e585ff27ea
Matthew Novelli <mattnovelli@gmail.com>
2019-04-17 18:52:28 -0500

    Add files via upload

---
+++ antieverything.py
@@ -0,0 +1,20 @@
+﻿import emoji
+import tweepy
+import io
+from emojis import output
+
+C_KEY = " "
+C_SECRET = " "
+A_TOKEN = " "
+A_TOKEN_SECRET = " "
+
+auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+api = tweepy.API(auth)
+api.update_status(output + "️‍⃠")
+
+L = (output + "\n")
+with io.open("used.txt","a", encoding='utf8') as f:
+    f.writelines(L)
+
+#print(s + "️‍⃠")
